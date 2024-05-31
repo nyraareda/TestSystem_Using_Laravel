64a897bfbd0a23e61b17c509270c2172f85506ed
nyraareda <nyraareda3@gmail.com>
2024-05-31 12:12:39 +0300

    test unit for login validation

--- tests/Unit/UserLoginTest.php
+++ tests/Unit/UserLoginTest.php
@@ -2,18 +2,33 @@
 
 namespace Tests\Unit;
 
-use PHPUnit\Framework\TestCase;
+use Tests\TestCase;
 use App\Models\User;
-
 
 class UserLoginTest extends TestCase
 {
     /**
      * A basic unit test example.
      */
-    public function user_login_form(): void
+    public function test_user_login_form(): void
     {
         $user = User::factory()->create();
         $response = $this->actingAs($user)->get('/login');
+        $response->assertStatus(302);
+
+    }
+
+    public function test_user_validation(): void
+    {
+        $user1 = User::make([
+            'name' => "nyraa reda",
+            'email' => "nyraa@gmail.com"
+        ]);
+        $user2 = User::make([
+            'name' => "esraa reda",
+            'email' => "esraa@gmail.com"
+        ]);
+
+        $this->assertTrue($user1->name != $user2->name);
     }
 }
